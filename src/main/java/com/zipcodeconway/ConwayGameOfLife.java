e1ef64dcad0d064811e44dd56159888bc90e80ee
Ashley Bloxom <ashley.bloxom@gmail.com>
2019-04-26 20:35:30 -0400

    created game of life with wrapping matrix edges

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,32 +1,73 @@
 package com.zipcodeconway;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConwayGameOfLife {
 
-    public ConwayGameOfLife(Integer dimension) {
-     }
+    private Integer dimension;
+    private Integer[][] startmatrix;
 
-    public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+    public ConwayGameOfLife(Integer dimension) {
+        this.dimension = dimension;
+        this.startmatrix = createRandomStart(dimension);
+    }
+
+    public ConwayGameOfLife(Integer dimension, Integer[][] startmatrix) {
+        this.dimension = dimension;
+        this.startmatrix = startmatrix;
+    }
+
+    public Integer[][] getStartmatrix() {
+        return startmatrix;
     }
 
     public static void main(String[] args) {
+        SimpleWindow simpleWindow = new SimpleWindow(50);
         ConwayGameOfLife sim = new ConwayGameOfLife(50);
-        int[][] endingWorld = sim.simulate(50);
+        for (int i = 0; i < 50; i++) {
+            Integer[][] endingWorld = sim.simulate(i);
+            simpleWindow.sleep(500);
+            simpleWindow.display(endingWorld, 50);
+        }
     }
 
     // Contains the logic for the starting scenario.
     // Which cells are alive or dead in generation 0.
     // allocates and returns the starting matrix of size 'dimension'
-    private int[][] createRandomStart(Integer dimension) {
-        return new int[1][1];
+    private static Integer[][] createRandomStart(Integer dimension) {
+        Integer[][] startmatrix = new Integer[dimension][dimension];
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                startmatrix[i][j] = (int)(Math.random() * 2);
+            }
+        }
+        return startmatrix;
     }
 
-    public int[][] simulate(Integer maxGenerations) {
-        return new int[1][1];
+    public Integer[][] simulate(Integer maxGenerations) {
+        Integer[][] newgen = new Integer[dimension][dimension];
+
+        for (int h = 0; h < maxGenerations; h++) {
+            for (int i = 0; i < dimension; i++) {
+                for (int j = 0; j < dimension; j++) {
+                    newgen[i][j] = isAlive(i, j, startmatrix);
+                }
+            }
+            copyAndZeroOut(newgen, startmatrix);
+        }
+        return startmatrix;
     }
 
     // copy the values of 'next' matrix to 'current' matrix,
     // and then zero out the contents of 'next' matrix
-    public void copyAndZeroOut(int [][] next, int[][] current) {
+    public void copyAndZeroOut(Integer[][] next, Integer[][] current) {
+        for (int i = 0; i < dimension; i++) {
+            for (int j = 0; j < dimension; j++) {
+                current[i][j] = next[i][j];
+                next[i][j] = 0;
+            }
+        }
     }
 
     // Calculate if an individual cell should be alive in the next generation.
@@ -37,7 +78,52 @@ public class ConwayGameOfLife {
 		Any live cell with two or three live neighbours lives, unchanged, to the next generation.
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
-    private int isAlive(int row, int col, int[][] world) {
-        return 0;
+    public Integer isAlive(int row, int col, Integer[][] world) {
+        int aliveNeighbors = countAlive(findNeighbors(row, col, world));
+        if (aliveNeighbors < 2 || aliveNeighbors > 3) {
+            return 0;
+        } else if (aliveNeighbors == 2) {
+            return world[row][col];
+        } else {
+            return 1;
+        }
     }
+
+    public List<Integer> findNeighbors(int row, int col, Integer[][] world) {
+        List<Integer> neighbors = new ArrayList<Integer>();
+        for (int i = row - 1; i <= row + 1; i++) {
+            for (int j = col - 1; j <= col + 1; j++) {
+                int x = i;
+                int y = j;
+                if (x != row || y != col) {
+                    if (x == -1) {
+                        x = dimension - 1;
+                    } else if (x == dimension) {
+                        x = 0;
+                    }
+                    if (y == -1) {
+                        y = dimension - 1;
+                    } else if (y == dimension) {
+                        y = 0;
+                    }
+                    neighbors.add(world[x][y]);
+                }
+            }
+        }
+        return neighbors;
+    }
+
+
+    public Integer countAlive(List<Integer> neighbors) {
+        int alive = 0;
+        for (Integer neighbor : neighbors) {
+            if (neighbor == 1) {
+                alive++;
+            }
+        }
+        return alive;
+
+    }
+
 }
+
--- src/test/java/com/zipcodeconway/ConwayGameOfLifeTest.java
+++ src/test/java/com/zipcodeconway/ConwayGameOfLifeTest.java
@@ -1,6 +1,11 @@
 package com.zipcodeconway;
 
+import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,39 +13,237 @@ public class ConwayGameOfLifeTest {
 
     @Test
     public void runTest1() {
-        int[][] start = {
+        Integer[][] start = {
                 {0, 0, 0, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 0, 0, 0}};
-        int[][] expected = {
+        Integer[][] expected = {
                 {0, 0, 0, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 0, 0, 0}};
         ConwayGameOfLife sim = new ConwayGameOfLife(5, start);
-        int[][] results = sim.simulate(9);
+        Integer[][] results = sim.simulate(10);
+
         assertTrue(java.util.Arrays.deepEquals(results, expected));
     }
 
     @Test
     public void runTest2() {
-        int[][] start = {
+        Integer[][] start = {
                 {0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0},
                 {0, 1, 1, 1, 0},
                 {0, 0, 0, 0, 0},
                 {0, 0, 0, 0, 0}};
-        int[][] expected = {
+        Integer[][] expected = {
                 {0, 0, 0, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 1, 0, 0},
                 {0, 0, 0, 0, 0}};
         ConwayGameOfLife sim = new ConwayGameOfLife(5, start);
-        int[][] results = sim.simulate(10);
+        Integer[][] results = sim.simulate(9);
         assertTrue(java.util.Arrays.deepEquals(results, expected));
     }
+
+    @Test
+    public void testFindNeighbors() {
+        Integer[][] arr = {
+                {0, 0, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(0, 0, 1, 1, 0, 0, 0, 0));
+
+        List<Integer> actual = conwayGameOfLife.findNeighbors(1, 1, arr);
+
+        for (int i = 0; i < expected.size(); i++) {
+            System.out.println(actual.get(i));
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void testFindNeighbors1() {
+        Integer[][] arr = {
+                {0, 0, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(0, 1, 0, 1, 1, 0, 0, 0));
+
+        List<Integer> actual = conwayGameOfLife.findNeighbors(1, 2, arr);
+
+        for (int i = 0; i < expected.size(); i++) {
+            System.out.println(actual.get(i));
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void testFindNeighbors2() {
+        Integer[][] arr = {
+                {0, 0, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(0, 1, 0, 1, 1, 0, 0, 0));
+
+        List<Integer> actual = conwayGameOfLife.findNeighbors(1, 2, arr);
+
+        for (int i = 0; i < expected.size(); i++) {
+            System.out.println(actual.get(i));
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void testFindNeighbors3() {
+        Integer[][] arr = {
+                {1, 0, 1},
+                {0, 1, 1},
+                {0, 1, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+        List<Integer> expected = new ArrayList<>(Arrays.asList(1, 1, 0, 1, 1, 0, 0, 1));
+
+        List<Integer> actual = conwayGameOfLife.findNeighbors(1, 0, arr);
+
+        for (int i = 0; i < expected.size(); i++) {
+            System.out.println(actual.get(i));
+            Assert.assertEquals(expected.get(i), actual.get(i));
+        }
+    }
+
+    @Test
+    public void testCopyAndZeroOut() {
+        Integer[][] current = {
+                {0, 0, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+        Integer[][] next = {
+                {0, 1, 0},
+                {0, 1, 0},
+                {0, 0, 0}};
+        Integer[][] copyofnext = {
+                {0, 1, 0},
+                {0, 1, 0},
+                {0, 0, 0}};
+        Integer[][] expected = {
+                {0, 0, 0},
+                {0, 0, 0},
+                {0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, current);
+
+        conwayGameOfLife.copyAndZeroOut(next, current);
+
+        Assert.assertArrayEquals("current should be a copy of next", current, copyofnext);
+
+        Assert.assertArrayEquals(expected, next);
+    }
+
+    @Test
+    public void testCountAlive() {
+        int expected = 2;
+        Integer[][] arr = {
+                {0, 0, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+
+        int actual = conwayGameOfLife.countAlive(conwayGameOfLife.findNeighbors(1, 1, arr));
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsAlive() {
+        int expected = 1;
+        Integer[][] arr = {
+                {0, 0, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+
+        int actual = conwayGameOfLife.isAlive(1, 1, arr);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsAlive1() {
+        int expected = 0;
+        Integer[][] arr = {
+                {0, 1, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+
+        int actual = conwayGameOfLife.isAlive(1, 2, arr);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testIsAlive2() {
+        int expected = 0;
+        Integer[][] arr = {
+                {0, 1, 1},
+                {1, 1, 0},
+                {0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(3, arr);
+
+        int actual = conwayGameOfLife.isAlive(2, 1, arr);
+
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testSimulate() {
+        Integer[][] arr = {
+                {1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+
+        Integer[][] expected = {
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
+        ConwayGameOfLife conwayGameOfLife = new ConwayGameOfLife(15, arr);
+
+
+        Integer[][] actual = conwayGameOfLife.simulate(5);
+
+        Assert.assertArrayEquals(expected, actual);
+    }
 }
+
